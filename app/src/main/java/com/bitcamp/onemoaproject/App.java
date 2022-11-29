6c6a01b34c97ce2b2273029158be971a94810fd1
LimDongHyun99 <ldh9036@naver.com>
2022-11-29 20:25:40 +0900

    .

---
+++ app/src/main/java/com/bitcamp/onemoaproject/App.java
@@ -0,0 +1,46 @@
+package com.bitcamp.onemoaproject;
+
+import com.bitcamp.onemoaproject.service.MemberService;
+import com.bitcamp.onemoaproject.service.order.OrderReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.bitcamp.onemoaproject.service.ContestService;
+
+@Controller
+@EnableTransactionManagement
+@SpringBootApplication
+
+public class App {
+
+  @Autowired
+  ContestService contestService;
+  
+  @Autowired
+  OrderReviewService orderReviewService;
+  
+  @Autowired
+  MemberService memberService;
+
+  public static void main(String[] args) {
+    System.out.println("비트캠프 프로젝트!");
+    SpringApplication.run(App.class, args);
+  }
+
+
+  @GetMapping("/")
+  public String welcome(@CookieValue(name = "email", defaultValue = "") String email, Model model) {
+    model.addAttribute("email", email);
+    model.addAttribute("contests", contestService.listMain());
+    model.addAttribute("reviews", orderReviewService.listMainReview());
+    System.out.println("reviews = " + orderReviewService.listMainReview());
+    return "index";
+  }
+}
+
+
---
+++ app/src/main/java/com/bitcamp/onemoaproject/vo/Member.java
@@ -0,0 +1,34 @@
+package com.bitcamp.onemoaproject.vo;
+
+import com.bitcamp.onemoaproject.vo.portfolio.Portfolio;
+import java.sql.Date;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Member {
+
+  private int no;
+  private String email;
+  private String password;
+  private String nickname;
+  private String name;
+  private String tel;
+  private Date createdDate;
+  private String postNo;
+  private String baseAddr;
+  private String detailAddr;
+  private int jobNo;
+  private String bank;
+  private String account;
+  private String profile;
+  private String token;
+  private int status;
+  private String interests;
+  
+  private List<Portfolio> portfoliosList;
+}
